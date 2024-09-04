7f753bb2c56e437e430253fc30c27a93ac5fba5f
reindorfn <reindorfnarh@gmail.com>
2024-09-04 01:49:29 +0000

    Configured streamlit setup. Will start the interface soon.

---
+++ App.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# import AshesiLeague as AL
+
+
+if __name__ == "__main__":
+    st.write("Hello Streamlit")
+    st.success("App runs successfully. It's now upto you to be creative Developer🤭")
+    
