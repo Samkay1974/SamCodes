4e47dc336aaf93cb17d9454e9e6297b5ac94a0be
reindorfn <reindorfnarh@gmail.com>
2024-09-10 11:37:37 +0000

    Updates on application

--- App.py
+++ App.py
@@ -6,3 +6,21 @@ if __name__ == "__main__":
     st.write("Hello Streamlit")
     st.success("App runs successfully. It's now upto you to be creative Developer🤭")
     
+    st.sidebar.button("Home")
+    st.sidebar.button("Teams")
+    st.sidebar.button("Groups")
+    
+
+
+def page2():
+    st.title("Second page")
+
+def page1():
+    st.title("First Page")
+
+pg = st.navigation([
+    st.Page(page1, title="First page", icon="🔥"),
+    st.Page(page2, title="Second page", icon=":material/favorite:"),
+])
+if page1:
+    st.switch_page("Pages/Teams.py")
---
+++ Pages/Teams.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Welcome!! This is the teams Page")
